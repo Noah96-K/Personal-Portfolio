4902e10d4f37d0996a71dd4929a06ab207316de9
Noah <khj2235720@naver.com>
2021-12-16 01:52:45 +0900

    Update naverlogin.py

--- webCrawlerPython/naverCrawler/naverlogin.py
+++ webCrawlerPython/naverCrawler/naverlogin.py
@@ -11,19 +11,20 @@ browser.maximize_window() #화면 최대화
 #웹사이트 열기
 browser.get(url)
 
-ID=browser.find_element_by_css_selector("#id")
+id=browser.find_element_by_css_selector("#id")
 # ID.send_keys(USERID) this method -> they will know that it was mecro
-pyperclip.copy("id")
-pyautogui.hotkey("ctrl", "v")
-time.sleep(3) #wait
+pyperclip.copy("id123")
+pyautogui.hotkey("command", "v")
+time.sleep(2) #wait
 
-pw=browser.find_element_by_id('pw')
+pw=browser.find_element_by_css_selector('#pw')
 pw.click()
-pyperclip.copy("pas@")
-pyautogui.hotkey("ctrl", "v")
+pyperclip.copy("ps1234")
+pyautogui.hotkey("command", "v")
 # pw.send_keys(USERPASSWORD) -> they will know that it was mecro
 
 time.sleep(3) #wait
 
-ID.send_keys(Keys.ENTER)
+login_btn=browser.find_element_by_css_selector("#log\.login")
+login_btn.click()
 
