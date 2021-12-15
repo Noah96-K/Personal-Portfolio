2dec14e4e999bc4e7b52736167ad86fb1b668a09
Noah <khj2235720@naver.com>
2021-12-16 01:29:48 +0900

    Update naverlogin.py

--- webCrawlerPython/naverCrawler/naverlogin.py
+++ webCrawlerPython/naverCrawler/naverlogin.py
@@ -1,31 +1,29 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys #for the enter function
-import csv
+import pyautogui
+import pyperclip
 
-#브라우저 생성
+url="https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com"
 browser=webdriver.Chrome('/Users/noah/Documents/chromedriver')
-
-#웹사이트 열기
-browser.get('https://www.naver.com')
 browser.implicitly_wait(10) #로딩이 끝날 때까지 10초 기다림
+browser.maximize_window() #화면 최대화
+#웹사이트 열기
+browser.get(url)
 
-browser.find_element_by_css_selector('.link_login').click()
-time.sleep(2) #wait
+ID=browser.find_element_by_css_selector("#id")
+# ID.send_keys(USERID) this method -> they will know that it was mecro
+pyperclip.copy("id")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(3) #wait
 
-#browser.send_keys('아asdf')
+pw=browser.find_element_by_id('pw')
+pw.click()
+pyperclip.copy("pas@")
+pyautogui.hotkey("ctrl", "v")
+# pw.send_keys(USERPASSWORD) -> they will know that it was mecro
 
-ID=browser.find_element_by_id('id')
-ID.send_keys('adfsdfasf')
-
-
-search=browser.find_element_by_id('pw')
-search.click()
-search.send_keys('adfsdfasf')
-
+time.sleep(3) #wait
 
 ID.send_keys(Keys.ENTER)
-
-
-browser=webdriver.Chrome('/Users/noah/Documents/chromedriver')
 
